c0273737a2a400e3f8b637be87c718fcbc9b5f9e
Shrivalli <shrivalli04@gmail.com>
2026-09-08 11:20:22 +0530

    Add JsonProperty annotation to aId field

--- src/main/java/com/cts/dto/AchievementDto.java
+++ src/main/java/com/cts/dto/AchievementDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AchievementDto {
+    @JsonProperty("aId")
     private Integer aId;
 
     private String type; // "ACHIEVEMENT" or "ACTIVITY"
