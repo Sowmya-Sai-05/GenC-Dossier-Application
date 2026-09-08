4bec90cb73e43c504eae4378b8f82862aa557798
shrivalli <2399530@cognizant.com>
2026-09-08 11:31:06 +0530

    fix: add missing JsonProperty import in Achievement and AchievementDto

--- src/main/java/com/cts/entity/Achievement.java
+++ src/main/java/com/cts/entity/Achievement.java
@@ -1,6 +1,7 @@
 package com.cts.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
